50f3fc65bdf0a69f696e8b2c94e9ebf01c865497
ermcgonagle <ermcgonagle>
2024-12-12 17:30:25 -0600

    initial skeleton for ticket info page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
+import React from 'react';
 import Hello from './components/Hello'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import TicketInfo from './components/TicketInfo';
 
 function App() {
   const func = () => {
@@ -6,11 +9,37 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <h1 className="text-6xl text-red-500">ACE Chilifest</h1>
-      <button className="bg-purple-500 hover:bg-blue-700 text-white" onClick={func}>.env test</button>
-      <Hello></Hello>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/ticket-info">Ticket Info</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="App">
+                <h1 className="text-6xl text-red-500">ACE Chilifest</h1>
+                <button
+                  className="bg-purple-500 hover:bg-blue-700 text-white"
+                  onClick={func}
+                >
+                  .env test
+                </button>
+                <Hello />
+              </div>
+            }
+          />
+          <Route path="/ticket-info" element={<TicketInfo />} />
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/TicketInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TicketInfo() {
+    return (
+        <div>
+            <div className='underline'>What's Included with My Ticket?</div>
+            <div className='italic'>Insert graphic over what is included in ticket here</div>
+        </div>
+    );
+}
+
+export default TicketInfo;
