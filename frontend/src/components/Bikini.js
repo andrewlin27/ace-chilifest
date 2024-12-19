053867efd715080a3d93e571a0377901ea394128
ermcgonagle <ermcgonagle>
2024-12-18 19:20:25 -0600

    initial bikini bottoms up design

--- frontend/src/components/Bikini.js
+++ frontend/src/components/Bikini.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-const images = [
-    'bikini_1.jpeg',
-]
-
 function Bikini() {
     return (
         <div className='relative bg-pink h-screen flex justify-center items-center'>
-            <div>
-                Bikini Bottoms Up
+
+            <div className="text-5xl sm:text-7xl font-extrabold uppercase tracking-wider text-spongeYellow text-center">
+                <span className="drop-shadow-[4px_4px_0_rgb(0,0,255)] skew-y-3 skew-x-3">Bikini</span> <br />
+                <span className="drop-shadow-[4px_4px_0_rgb(255,0,0)] skew-y-3 skew-x-3">Bottoms Up</span> <br />
+                <span className="drop-shadow-[4px_4px_0_rgb(255,255,0)] skew-y-3 skew-x-3">2023</span> <br />
             </div>
         </div>
     );
