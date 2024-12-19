a39a07adf20b2abc0bfbcc8aaddc71def1acddfa
Andrew Lin <andrew.mlin27@gmail.com>
2024-12-19 00:47:09 -0600

    more stripe buttons

--- frontend/src/components/Tickets.js
+++ frontend/src/components/Tickets.js
@@ -17,6 +17,20 @@ const Tickets = () => {
                 >
             </stripe-buy-button>
         </div>
+        <div className="border-2 border-black p-2 rounded-md">
+            <stripe-buy-button
+                buy-button-id="buy_btn_1QXZJqK7YftLNASt4eSZkqM8"
+                publishable-key="pk_live_51QXBW9K7YftLNASt6Lhrq3FtAOmPkKlyC9QjRbtHGHXiJSCOyVqmy5jyjCbe6ZkBxuRHlstTUYilVOc8DWms3Td900Eq9lzcXN"
+                >
+            </stripe-buy-button>
+        </div>
+        <div className="border-2 border-black p-2 rounded-md">
+            <stripe-buy-button
+                buy-button-id="buy_btn_1QXZObK7YftLNASt5AetDWvt"
+                publishable-key="pk_live_51QXBW9K7YftLNASt6Lhrq3FtAOmPkKlyC9QjRbtHGHXiJSCOyVqmy5jyjCbe6ZkBxuRHlstTUYilVOc8DWms3Td900Eq9lzcXN"
+                >
+            </stripe-buy-button>
+        </div>
     </div>
   )
 }
