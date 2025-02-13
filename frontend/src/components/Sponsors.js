f9a89e22706d2f8e0c9bf79812d86ff3ad7cd458
Andrew Lin <andrew.mlin27@gmail.com>
2025-02-13 13:28:46 -0600

    new sponsors

--- frontend/src/components/Sponsors.js
+++ frontend/src/components/Sponsors.js
@@ -3,6 +3,9 @@ import React from 'react'
 const Sponsors = () => {
     const sponsors = [
         { id: 1, name: 'West Campus Tees', logo: 'wct.png' },
+        { id: 2, name: 'Twisted Tea', logo: 'twea.png' },
+        { id: 3, name: 'Keystone Light', logo: 'keystone.png' },
+
     ];
 
   return (
@@ -10,7 +13,7 @@ const Sponsors = () => {
         <h2 className="text-4xl font-bold mb-8">Thank you to our Sponsors!</h2>
         <div className="flex flex-wrap justify-center items-center gap-8">
             {sponsors.map((sponsor) => (
-            <div key={sponsor.id} className="w-40 h-40 flex items-center justify-center bg-white shadow-lg rounded-md">
+            <div key={sponsor.id} className="border border-black w-40 h-40 flex items-center justify-center bg-white shadow-lg rounded-md">
                 <img
                     src={sponsor.logo}
                     alt={sponsor.name}
